b123892445f7b0f11f08be447a268d5798d1e8c1
Chetan Harjani <chetonix@gmail.com>
2021-11-04 17:03:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// const closeModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  // console.log('Button clicked!');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //console.log('A key was pressed!');
+  //console.log(e);
+  if (e.key === 'Escape') {
+    //console.log(e.key);
+
+    if (!modal.classList.contains('hidden')) {
+      //The two ifs can be combined into one using && operator
+
+      // modal.classList.add('hidden');
+      // overlay.classList.add('hidden');
+
+      closeModal();
+    }
+  }
+});
